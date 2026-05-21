8d40da1c7cea1f8ea9021b65e2ce353bc02d1dc9
atlibby <1lampart2000@gmail.com>
2026-05-21 19:23:09 -0400

    Added constructors

--- backend/trading-api/trading-api/src/main/java/com/tradingsim/tradingapi/dto/CreateOrderRequest.java
+++ backend/trading-api/trading-api/src/main/java/com/tradingsim/tradingapi/dto/CreateOrderRequest.java
@@ -4,8 +4,19 @@ import java.util.UUID;
 import com.tradingsim.tradingapi.model.OrderSide;
 
 public class CreateOrderRequest {
-    UUID userID;
-    String symbol;
-    OrderSide side;
-    int quantity;
+    private UUID userID;
+    private String symbol;
+    private OrderSide side;
+    private int quantity;
+
+    public CreateOrderRequest(){}
+
+    public CreateOrderRequest(UUID userID, String symbol, OrderSide side, int quantity){
+        this.userID = userID;
+        this.symbol = symbol;
+        this.side = side;
+        this.quantity = quantity;
+    }
 }
+
+
