e888ed55df6c7109fa57534de093410cc2ff729b
atlibby <1lampart2000@gmail.com>
2026-05-21 19:50:09 -0400

    Added constructors

--- backend/trading-api/trading-api/src/main/java/com/tradingsim/tradingapi/dto/CreateOrderResponse.java
+++ backend/trading-api/trading-api/src/main/java/com/tradingsim/tradingapi/dto/CreateOrderResponse.java
@@ -1,5 +1,19 @@
 package com.tradingsim.tradingapi.dto;
+import java.util.UUID;
+
 
 public class CreateOrderResponse {
+    private UUID orderID;
+    private String status; // TODO: Consider changing data type later
+    private String message;
+
+    public CreateOrderResponse(){}
+
+    public CreateOrderResponse(UUID orderID, String status, String message){
+        this.orderID = orderID;
+        this.status = status;
+        this.message = message;
+    }
+
     
 }
