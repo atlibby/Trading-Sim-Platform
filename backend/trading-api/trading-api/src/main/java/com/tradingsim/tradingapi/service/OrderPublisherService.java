8d548fb4879ba0f9d1338bfee652d91050bd2aa9
atlibby <1lampart2000@gmail.com>
2026-05-21 20:53:34 -0400

    Filled out OrderPublisherService.java

--- backend/trading-api/trading-api/src/main/java/com/tradingsim/tradingapi/service/OrderPublisherService.java
+++ backend/trading-api/trading-api/src/main/java/com/tradingsim/tradingapi/service/OrderPublisherService.java
@@ -1,13 +1,31 @@
 package com.tradingsim.tradingapi.service;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import java.util.UUID;
+
+import com.tradingsim.tradingapi.events.OrderCreated;
+
+@Service
 public class OrderPublisherService {
     // TODO: inject KafkaTemplate<String, OrderCreated>
+    private final KafkaTemplate<String, OrderCreated> kafkaTemplate;
+    private final OrderCreated event;
+
+    public OrderPublisherService(KafkaTemplate<String, OrderCreated> kafkaTemplate, OrderCreated event){
+        this.kafkaTemplate = kafkaTemplate;
+        this.event = event;
+    }
 
     public void publishOrderCreated(OrderCreated event){
         // TODO: send event to topic "orders.created"
+        UUID orderID = event.getOrderID();
 
         // TODO: use orderID as message key
+        kafkaTemplate.send("orders.created", orderID.toString(), event);
 
         // TODO: print/log published event for now
+        System.out.println("Published order: " + event.getSymbol() + " " + event.getUserID().toString());
     }
 }
