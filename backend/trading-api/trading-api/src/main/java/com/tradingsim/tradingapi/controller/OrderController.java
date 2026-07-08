b7485921200963f66735436965a7a2eebede9cf8
atlibby <1lampart2000@gmail.com>
2026-07-07 23:02:16 -0400

    ResponseEntity added to OrderController

--- backend/trading-api/trading-api/src/main/java/com/tradingsim/tradingapi/controller/OrderController.java
+++ backend/trading-api/trading-api/src/main/java/com/tradingsim/tradingapi/controller/OrderController.java
@@ -1,12 +1,16 @@
 package com.tradingsim.tradingapi.controller;
 import com.tradingsim.tradingapi.events.OrderCreated;
-import com.tradingsim.tradingapi.dto.*;
+import com.tradingsim.tradingapi.dto.CreateOrderRequest;
+import com.tradingsim.tradingapi.dto.CreateOrderResponse;
 import com.tradingsim.tradingapi.service.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 /**Goal (general):
  * POST api/orders
@@ -28,16 +32,20 @@ public class OrderController {
     private final OrderPublisherService orderPublisher;
     private final OrderValidationService orderValidator;
 
-    private final CreateOrderRequest orderRequest;
-
-    public OrderController(OrderPublisherService orderPublisher, OrderValidationService orderValidator, CreateOrderRequest orderRequest){
+    public OrderController(OrderPublisherService orderPublisher, OrderValidationService orderValidator){
         this.orderPublisher = orderPublisher;
         this.orderValidator = orderValidator;
-        this.orderRequest = orderRequest;
     }
+    
 
-    public boolean validateRequest(CreateOrderRequest orderRequest){
-        // TODO: inject validation service logic here
-        OrderValidationService orderValidator;
+    @PostMapping
+    public ResponseEntity<CreateOrderResponse> createOrder(
+        @RequestBody CreateOrderRequest request 
+    ){
+        orderPublisher.publishOrderCreated();
+        return orderValidator.validateRequest(request.getQuantity(), request.getUserID(), request.getSide());
     }
+    
 }
+
+
